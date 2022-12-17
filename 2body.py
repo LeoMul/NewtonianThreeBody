d2e35bf6affeac101d00da0a95f47d65e5644cf1
Leo Mulholland <leomulholland@hotmail.com>
2022-12-17 22:52:41 +0000

    working now

--- 2body.py
+++ 2body.py
@@ -7,39 +7,30 @@ import sys
 #planet1: E
 #planet2: M
 
-masssun = 330000
-massmoon = 0.0123031469
+masssun = 3
 # 0  1       2     3      4      5  
 #S_x S_y     E_x   E_y    M_x M_y
 
 def calculate_accelerations(positions):
-    #assume positions is an np array of size 6
-    accelerations = np.zeros(6)
+    #assume positions is an np array of size 4
+    accelerations = np.zeros(4)
 
-    distanceMS = np.sqrt((positions[4] - positions[0])**2 + (positions[5] - positions[1])**2)
     distanceES = np.sqrt((positions[2] - positions[0])**2 + (positions[3] - positions[1])**2)
-    distanceEM = np.sqrt((positions[2] - positions[4])**2 + (positions[3] - positions[5])**2)
 
 
 
-    distanceMSpminus3 = distanceMS**(-3)
     distanceESpminus3 = distanceES**(-3)
-    distanceEMpminus3 = distanceEM**(-3)
     #print("Positions:")
     #print(positions)
     #print("distances to power -3")
     #print(distanceMSpminus3,distanceESpminus3,distanceEMpminus3)
 
-    accelerations[0] = - distanceESpminus3 * (positions[0]-positions[2]) - (positions[0]-positions[4])*distanceMSpminus3*massmoon
-    accelerations[1] = - distanceESpminus3 * (positions[1]-positions[3]) - (positions[1]-positions[5])*distanceMSpminus3*massmoon
+    accelerations[0] = - distanceESpminus3 * (positions[0]-positions[2]) 
+    accelerations[1] = - distanceESpminus3 * (positions[1]-positions[3]) 
     
-    accelerations[2] = - distanceESpminus3 * (positions[2]-positions[0])*masssun - distanceEMpminus3 * (positions[2]-positions[4])*massmoon
-    accelerations[3] = - distanceESpminus3 * (positions[3]-positions[1])*masssun - distanceEMpminus3 * (positions[3]-positions[5])*massmoon
-    
-    accelerations[4] = - distanceMSpminus3 * (positions[4]-positions[0])*masssun - distanceEMpminus3 * (positions[4]-positions[2])
-    accelerations[5] = - distanceMSpminus3 * (positions[5]-positions[1])*masssun -  distanceEMpminus3 * (positions[5]-positions[3])
-
-    
+    accelerations[2] = -distanceESpminus3 * (positions[2]-positions[0])*masssun 
+    accelerations[3] = -distanceESpminus3 * (positions[3]-positions[1])*masssun 
+     
     #print(accelerations)
 
     return accelerations
@@ -59,26 +50,19 @@ def next_verlet(positions,positions_minus_one,delta_t):
 
 def render(positions):
     screen.fill(WHITE)
-    scale = 200
-    moonscale = 50.0
+    scale = 50
+
     x_s = w/2 + scale*positions[0]
     y_s = h/2 + scale*positions[1]
 
     x_e = w/2 + scale*positions[2]
     y_e = h/2 + scale*positions[3]
-    #x_m = w/2 + scale*positions[4]
-    #y_m = h/2 + scale*positions[5]
 
-    x_m = x_e + moonscale*scale*(positions[4]-positions[2])
-    y_m = y_e + moonscale*scale*(positions[5]-positions[3])
-
-
-    pygame.draw.circle(screen,BLACK,(x_s,y_s),20)
+    pygame.draw.circle(screen,BLACK,(x_s,y_s),10)
     pygame.draw.circle(screen,RED,(x_e,y_e),10)
-    pygame.draw.circle(screen,BLUE,(x_m,y_m),5)
 
 
-w,h = 1400,700
+w,h = 800,480
 origin = (w/2,h/2)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
@@ -90,9 +74,9 @@ screen.fill(WHITE)
 pygame.display.update()
 clock = pygame.time.Clock()
 
-positions = np.array([0,0,1.0,0.0,1.0,0.002569])
-delta_t = 1.0e-5
-initial_velocities = np.array([0.0,0.0,0.0,np.sqrt(masssun),np.sqrt(masssun)/30.0,np.sqrt(masssun)])
+positions = np.array([0,0,1,0])
+delta_t = 1.0e-3
+initial_velocities = np.array([0.0,0.0,0.0,np.sqrt(masssun)])
 
 positions_minus_one = positions
 positions = first_step(positions_minus_one,initial_velocities,delta_t)
--- test_anim.py
+++ test_anim.py
@@ -1,10 +1,27 @@
 import pygame 
 from pygame.locals import *
+import sys 
+
+def render():
+    pygame.draw.circle(screen,BLACK,origin,10)
+
 
 w,h = 800,480
+origin = (w/2,h/2)
 WHITE = (255,255,255)
 BLACK = (0,0,0)
 RED = (255,0,0)
 BLUE = (0,255,0)
 
-screen = pygame.display.sey_mode((w,h))
+screen = pygame.display.set_mode((w,h))
+screen.fill(WHITE)
+pygame.display.update()
+clock = pygame.time.Clock()
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()  
+    clock.tick(60)
+    pygame.display.update()
+    render()
